77849da8573ecb7556ed205cd9da3989a660a60e
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-12 20:15:36 -0300

    feat: added routes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#7159c1',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#fff',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to RocketShoes!</Text>
-      <Text style={styles.welcome}>Under</Text>
-    </View>
-  );
+  return <Routes />;
 }
