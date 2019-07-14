1285302c2e944e2275574913d861ebc52b91a125
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-14 00:49:52 -0300

    fix: header now hows correct cart size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Wrapper, Container, Logo, CartContainer, ItemCount } from './styles';
 
-function Header({ navigation }) {
+function Header({ navigation, cartSize }) {
   return (
     <Wrapper>
       <Container>
         <Logo />
         <CartContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#fff" size={24} />
-          <ItemCount>3</ItemCount>
+          <ItemCount>{cartSize || 0}</ItemCount>
         </CartContainer>
       </Container>
     </Wrapper>
   );
 }
 
-export default Header;
+export default connect(
+  state => ({
+    cartSize: state.cart.length,
+  }),
+  null
+)(Header);
